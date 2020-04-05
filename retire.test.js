98774532eda71dae3e12efc3c6db57e752a2c10a
ElijahBenMagee <48970303+ElijahBenMagee@users.noreply.github.com>
2020-04-04 23:24:40 -0500

    Update retire.test.js

--- retire.test.js
+++ retire.test.js
@@ -1,5 +1,9 @@
 const testVar = require('./retire');
 
-test('adds 1 + 2 to equal 3', () => {
+test('Test 1', () => {
   expect(testVar(1000,50,50,50)).toBe("Goal cannot be met. You will die first");
+});
+
+test('Test 2', () => {
+  expect(testVar("",50,50,50)).toBe("NaN years old.");
 });
