26be83998167c15319ef30a0457f678fac8a81af
ElijahBenMagee <48970303+ElijahBenMagee@users.noreply.github.com>
2020-04-04 22:48:57 -0500

    push

--- retire.js
+++ retire.js
@@ -1,48 +1,42 @@
 function retireForm(){
-	 
-         try{
+	document.getElementById("retireErr").innerHTML = retire(w,a,s,g);	 
+         //try{
 	     var w = parseFloat(document.getElementById("curSal").value);
 	     var a = parseFloat(document.getElementById("curAge").value);
 	     var s = parseFloat(document.getElementById("saved").value);
 	     var g = parseFloat(document.getElementById("goal").value);
-            //document.getElementById("bodyErr").innerHTML = h;      
-            //document.getElementById("bodyErr").innerHTML = w*.45/((0.025*h)**2);
-            if (a+g/(w*s*1.35/100) >= 100){
-		      document.getElementById("retireErr").innerHTML = "Goal cannot be met. You will die first";
-            }else{
+          
+         //   if (a+g/(w*s*1.35/100) >= 100){
+	 //	      document.getElementById("retireErr").innerHTML = "Goal cannot be met. You will die first";
+         //   }else{
 		document.getElementById("retireErr").innerHTML = a+g/(w*s*1.35/100) + " years old.";
-	    }
+	 //   }
 
 
          }
-	 catch(err){
-	      document.getElementById("retireErr").innerHTML = "Bad input, try again.";
-	      document.getElementById("retireErr").name = err.message;
+	 //catch(err){
+	 //     document.getElementById("retireErr").innerHTML = "Bad input, try again.";
+	      //document.getElementById("retireErr").name = err.message;
 	 
-         }
-	 return 3;
+         //}
+	 //return 3;
       }
 
-function retire(){
+function retire(w,a,s,g){
 	 
          try{
-	     var w = parseFloat(document.getElementById("curSal").value);
-	     var a = parseFloat(document.getElementById("curAge").value);
-	     var s = parseFloat(document.getElementById("saved").value);
-	     var g = parseFloat(document.getElementById("goal").value);
-            //document.getElementById("bodyErr").innerHTML = h;      
-            //document.getElementById("bodyErr").innerHTML = w*.45/((0.025*h)**2);
+	   
             if (a+g/(w*s*1.35/100) >= 100){
-		      document.getElementById("retireErr").innerHTML = "Goal cannot be met. You will die first";
+		      return"Goal cannot be met. You will die first";
             }else{
-		document.getElementById("retireErr").innerHTML = a+g/(w*s*1.35/100) + " years old.";
+		return  a+g/(w*s*1.35/100) + " years old.";
 	    }
 
 
          }
 	 catch(err){
-	      document.getElementById("retireErr").innerHTML = "Bad input, try again.";
-	      document.getElementById("retireErr").name = err.message;
+	      return "Bad input, try again.";
+	     
 	 
          }
 	 return 3;
--- retire.test.js
+++ retire.test.js
@@ -1,5 +1,5 @@
 const testVar = require('./retire');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(testVar()).toBe(3);
+  expect(testVar(1000,50,50,50)).toBe("Goal cannot be met. You will die first");
 });
